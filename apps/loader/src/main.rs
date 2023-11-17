168c1db7694999eab63034caadc221d3e8fd1e73
CheeseLongan <1131531950@qq.com>
2023-11-17 12:45:54 +0800

    exec 6 Complete

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -7,10 +7,14 @@ use axstd::println;
 #[cfg(feature = "axstd")]
 use axstd::process::exit;
 
+#[link_section = ".data.app_page_table"]
+static mut APP_PT_SV39: [u64; 512] = [0; 512];
+
 /// Head: 0 u8: app number, then two u8: app size, the size of u8...
 
 const PLASH_START: usize = 0x22000000;
-const RUN_START: usize = 0xffff_ffc0_8010_0000;
+// const RUN_START: usize = 0xffff_ffc0_8010_0000;
+const RUN_START: usize = 0x4010_0000;
 
 const SYS_HELLO: usize = 1;
 const SYS_PUTCHAR: usize = 2;
@@ -42,15 +46,24 @@ fn main() {
         num[0]
     };
 
-    println!("Load payload ...\n");
+    register_abi(SYS_HELLO, abi_hello as usize);
+    register_abi(SYS_PUTCHAR, abi_putchar as usize);
+    register_abi(SYS_TERMINATE, abi_terminate as usize);
+
     let mut start_now = PLASH_START + 1;
+
     for i in 0..apps_num {
+
+        unsafe { init_app_page_table(); }
+        unsafe { switch_app_aspace(i as usize); }
+
+        println!("Load payload ...");
         let size: &[u8] = unsafe { core::slice::from_raw_parts(start_now as *const u8, 2) };
 
         let apps_size = (((size[0] as usize) << 8) + size[1] as usize) as usize; 
         let apps_start = (start_now + 2) as *const u8;
         let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size) };
-        println!("app{}_content: {:?}; address: [{:?}]", i, code, code.as_ptr());
+        println!("app{}_size: {}; address: [{:?}]", i, code.len(), code.as_ptr());
         start_now += 2 + apps_size;
 
         // app running aspace
@@ -60,41 +73,41 @@ fn main() {
             core::slice::from_raw_parts_mut(RUN_START as *mut u8, apps_size)
         };
         run_code.copy_from_slice(code);
-        println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
+        println!("run code size {}; address [{:?}]", run_code.len(), run_code.as_ptr());
+        println!("Load payload ok!");
 
-        // println!("Execute app{} ...", i);
-        // // execute app
-        // unsafe { core::arch::asm!("
-        //     li t2, {run_start}
-        //     jalr t2",
-        //     run_start = const RUN_START,
-        // );}
-        // println!();
+        println!("Execute app{} ...", i);
+        // execute app
+        unsafe { core::arch::asm!("
+            la a0, {abi_table}
+            li t2, {run_start}
+            jalr t2",
+            run_start = const RUN_START,
+            abi_table = sym ABI_TABLE,
+        );}
+        println!();
 
     }
-    println!("Load payload ok!");
-    register_abi(SYS_HELLO, abi_hello as usize);
-    register_abi(SYS_PUTCHAR, abi_putchar as usize);
-    register_abi(SYS_TERMINATE, abi_terminate as usize);
 
-    println!("Execute app ...");
-    let arg0: u8 = b'A';
-    // execute app
-    unsafe { core::arch::asm!("
-        li t0, {abi_num}
-        slli t0, t0, 3
-        la t1, {abi_table}
-        add t1, t1, t0
-        ld t1, (t1)
-        jalr t1
-        li t2, {run_start}
-        jalr t2
-        j .",
-        run_start = const RUN_START,
-        abi_table = sym ABI_TABLE,
-        // abi_num = const SYS_HELLO,
-        // abi_num = const SYS_PUTCHAR,
-        abi_num = const SYS_TERMINATE,
-        in("a0") arg0,
-    )}
+    println!("Execute app done, total {} apps", apps_num);
+
+}
+
+unsafe fn init_app_page_table() {
+    // 0x8000_0000..0xc000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[2] = (0x80000 << 10) | 0xef;
+    // 0xffff_ffc0_8000_0000..0xffff_ffc0_c000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0x102] = (0x80000 << 10) | 0xef;
+    // 0x0000_0000..0x4000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0] = (0x00000 << 10) | 0xef;
+    // For App aspace!
+    // 0x4000_0000..0x8000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[1] = (0x80000 << 10) | 0xef;
+}
+unsafe fn switch_app_aspace(asid: usize) {
+    use riscv::register::satp;
+    let page_table_root = APP_PT_SV39.as_ptr() as usize -
+    axconfig::PHYS_VIRT_OFFSET;
+    satp::set(satp::Mode::Sv39, asid, page_table_root >> 12);
+    riscv::asm::sfence_vma_all();
 }
--- make.sh
+++ make.sh
@@ -15,13 +15,19 @@ dd if=/dev/zero of=./apps.bin bs=1M count=32
 app_num=2
 printf "$(printf '%02x' $app_num)" | xxd -r -p | dd of=apps.bin conv=notrunc bs=1 seek=0
 
+start=1
+
 app_size=$(stat -c %s ./hello_app/hello_app.bin)
-printf "$(printf '%04x' $app_size)" | xxd -r -p | dd of=./apps.bin conv=notrunc bs=1 seek=1
-dd if=./hello_app/hello_app.bin of=./apps.bin conv=notrunc bs=1 seek=3
+printf "$(printf '%04x' $app_size)" | xxd -r -p | dd of=./apps.bin conv=notrunc bs=1 seek=$start
+start=$(($start+2))
+dd if=./hello_app/hello_app.bin of=./apps.bin conv=notrunc bs=1 seek=$start
+start=$(($start+$app_size))
 
 app_size2=$(stat -c %s ./hello_app2/hello_app2.bin)
-printf "$(printf '%04x' $app_size2)" | xxd -r -p | dd of=./apps.bin conv=notrunc bs=1 seek=9
-dd if=./hello_app2/hello_app2.bin of=./apps.bin conv=notrunc bs=1 seek=11
+printf "$(printf '%04x' $app_size2)" | xxd -r -p | dd of=./apps.bin conv=notrunc bs=1 seek=$start
+start=$(($start+2))
+dd if=./hello_app2/hello_app2.bin of=./apps.bin conv=notrunc bs=1 seek=$start
+start=$(($start+$app_size))
 
 mkdir -p ./arceos/payload
 mv ./apps.bin ./arceos/payload/apps.bin
